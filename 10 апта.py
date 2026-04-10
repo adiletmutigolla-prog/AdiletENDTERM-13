30358afba047cc8f59d48b4b51ed5801c2e114db
Admin <adilet.mutigolla@narxoz.kz>
2026-04-10 14:15:19 +0500

    10 апта тапсырмасы

--- 10 апта.py
+++ 10 апта.py
@@ -8,5 +8,5 @@ courses_cleaned = courses.drop_duplicates(subset=['course_code']).sort_values('c
 merged_df = pd.merge(enroll, courses_cleaned, on='course_code')
 result = merged_df.groupby('title')['student_id'].count().reset_index()
 
-print("Сұрыпталған және тазартылған анықтамалық бойынша нәтиже:")
+print("Сұрыпталған және тазартылған анықтамалық бойынша нәтиже: ")
 print(result)
