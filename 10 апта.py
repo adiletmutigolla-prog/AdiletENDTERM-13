28a0263a6b7b000b36c2861c62fe94e8afd5b0f1
Admin <adilet.mutigolla@narxoz.kz>
2026-04-10 14:13:51 +0500

    9 апта тапсырмасы

---
+++ 10 апта.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+enroll = pd.read_csv('enroll.csv')
+courses = pd.read_csv('courses.csv')
+
+courses_cleaned = courses.drop_duplicates(subset=['course_code']).sort_values('course_code')
+
+merged_df = pd.merge(enroll, courses_cleaned, on='course_code')
+result = merged_df.groupby('title')['student_id'].count().reset_index()
+
+print("Сұрыпталған және тазартылған анықтамалық бойынша нәтиже:")
+print(result)
